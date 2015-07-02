ee9a8e70c7ed8715dba35ec336f0364e8d34ffc5
Reza <resal81@gmail.com>
2015-07-02 12:02:13 -0400

    updated atom.py

--- kymia/blocks/atom.py
+++ kymia/blocks/atom.py
@@ -3,27 +3,37 @@ This module provides Atom and AtomType classes.
 """
 
 
-class AtomImmutableProperty(AttributeError):
-    """ This is raised when a read-only property is
-        attempted to be changed.
-    """
+class ImmutableProperty(AttributeError):
+    """ Raised when a read-only property is attempted to be changed. """
+    pass
+
+
+class AttributeIsNotSet(ValueError):
+    """ Raised when a not-set attribute is requested. """
+    pass
+
+
+class WrongTypeForOverloading(ValueError):
+    """ Raised when the expected `other` in special methods is not of the same type. """
+    pass
+
 
 PDB_FORMAT = {
     'ATOM': dict(
-        aserial=slice(6, 11),
-        aname=slice(12, 16),
-        alt=slice(16, 17),
-        rname=slice(17, 21),
-        chain=slice(21, 22),
-        rserial=slice(22, 26),
-        coordx=slice(30, 38),
-        coordy=slice(38, 46),
-        coordz=slice(46, 54),
-        occupancy=slice(54, 60),
-        bfactor=slice(60, 66),
-        segment=slice(72, 76),
-        element=slice(76, 78),
-        charge=slice(78, 80)
+        aserial   = slice(6, 11),
+        aname     = slice(12, 16),
+        alt       = slice(16, 17),
+        rname     = slice(17, 21),
+        chain     = slice(21, 22),
+        rserial   = slice(22, 26),
+        coordx    = slice(30, 38),
+        coordy    = slice(38, 46),
+        coordz    = slice(46, 54),
+        occupancy = slice(54, 60),
+        bfactor   = slice(60, 66),
+        segment   = slice(72, 76),
+        element   = slice(76, 78),
+        charge    = slice(78, 80)
     )
 }
 
@@ -32,35 +42,59 @@ class AtomType:
     def __init__(self, label: str):
         """ AtomType. Once you set an attribute, it becomes read-only. """
 
-        self._label = label
-        self._protons = None
-        self._mass = None
-        self._lj_dist = None
-        self._lj_energy = None
-        self._lj_dist14 = None
-        self._lj_energy14 = None
-        self._charge = None
-        self._partial_charge = None
-        self._radius = None
+        self.__label          = label
+        self.__protons        = None
+        self.__mass           = None
+        self.__lj_dist        = None
+        self.__lj_energy      = None
+        self.__lj_dist14      = None
+        self.__lj_energy14    = None
+        self.__charge         = None
+        self.__partial_charge = None
+        self.__radius         = None
+
+    # ----------------------------
+    # laebel
+    # ----------------------------
+
+    @property
+    def label():
+        return self.__label
+
+    @label.setter
+    def label(self, _):
+        raise ImmutableProperty
+
+    # ----------------------------
+    # special methods
+    # ----------------------------
+
+    def __eq__(self, other):
+        if not isinstance(other, AtomType):
+            raise WrongTypeForOverloading
+
+        return self.label == other.label
+
+    def __hash__(self):
+        return hash(self.label)
 
 
 class Atom:
     def __init__(self, name: str, serial: int):
         """ Atom class. Once you set each attribute, it becomes read-only. """
 
-        self._name = name
-        self._serial = serial
-        self._bfactor = None
-        self._occupancy = None
-        self._alt_loc = None
-        self._is_hetero = None
-        self._type = None
+        self.__name          = name
+        self.__serial        = serial
+        self.__bfactor       = None
+        self.__occupancy     = None
+        self.__alt_loc       = None
+        self.__is_hetero     = None
+        self.__type          = None
+        self.__source_format = None  # pdb, pqr, ...
 
-        self._source_format = None  # pdb, pqr, ...
-
-    # -------------
+    # ----------------------------
     # builders
-    # -------------
+    # ----------------------------
 
     @classmethod
     def from_pdb_line(cls, line):
@@ -70,37 +104,92 @@ class Atom:
     def from_pqr_line(cls, line):
         pass
 
-    # -------------
+    # ----------------------------
     # name
-    # -------------
+    # ----------------------------
 
     @property
     def name(self):
-        return self._name
+        return self.__name
 
     @name.setter
     def name(self, _):
-        raise AtomImmutableProperty("name is already set.")
+        raise ImmutableProperty
 
-    @name.deleter
-    def name(self):
-        raise AttributeError("name cannot be deleted")
+    # ----------------------------
+    # serial
+    # ----------------------------
 
-    # -------------
+    @property
+    def serial(self):
+        return self.__serial
+
+    @serial.setter
+    def serial(self, _):
+        raise ImmutableProperty
+
+    # ----------------------------
+    # type
+    # ----------------------------
+
+    @property
+    def type(self):
+        if self.__type is None:
+            raise AttributeIsNotSet
+        return self.__type
+
+    @type.setter
+    def type(self, val):
+        if self.__type is not None:
+            raise ImmutableProperty
+
+        if isinstance(val, AtomType):
+            raise WrongTypeForOverloading
+
+        self.__type = val
+
+    # ----------------------------
     # bfactor
-    # -------------
+    # ----------------------------
+
     @property
     def bfactor(self):
-        if self._bfactor is None:
-            raise ValueError("bfactor has not been set")
-        return self._bfactor
+        if self.__bfactor is None:
+            raise AttributeIsNotSet
+        return self.__bfactor
 
     @bfactor.setter
     def bfactor(self, val: float):
-        if self._bfactor is not None:
-            raise AttributeError("bfactor is already set.")
-        self._bfactor = val
+        if self.__bfactor is not None:
+            raise ImmutableProperty
+        self.__bfactor = val
 
-    @bfactor.deleter
-    def bfactor(self):
-        raise AttributeError("bfactor cannot be deleted")
+    # ----------------------------
+    # special methods
+    # ----------------------------
+
+    def __iter__(self):
+        """ Can be used for example in `name, serial = Atom('C', 12)`. """
+
+        return (i for i in (self.name, self.serial))
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return '{}({!r}, {!r})'.format(class_name, self.name, self.serial)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __eq__(self, other):
+        if not isinstance(other, Atom):
+            raise WrongTypeForOverloading
+
+        cmp = [getattr(self, v) == getattr(other, v) for v in ('name', 'serial', 'type')]
+        return all(cmp)
+
+    def __hash__(self):
+        return hash(self.name) ^ hash(self.type)
+
+
+
+
--- kymia/blocks/atom_test.py
+++ kymia/blocks/atom_test.py
@@ -1,12 +1,14 @@
 import pytest
 
-from .atom import Atom, AtomImmutableProperty
+from .atom import Atom, ImmutableProperty
+
 
 def test_atom_creation():
     a = Atom('C', 1)
     assert a.name == 'C'
 
+
 def test_atom_readonly():
     a = Atom('C', 1)
-    with pytest.raises(AtomImmutableProperty):
+    with pytest.raises(ImmutableProperty):
         a.name = 'B'
